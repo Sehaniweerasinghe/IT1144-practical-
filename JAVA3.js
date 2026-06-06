a2fd9b05bc8fcaab5ef0244b57f79fa0d83c6df0
Sehaniweerasinghe <sehaniweerasinghe4@gmail.com>
2026-06-06 12:40:59 +0530

    Add files via upload

---
+++ JAVA3.js
@@ -0,0 +1,4 @@
+const num=[1,2,3];
+for(let key of num){
+ console.log(key);
+} 
---
+++ cangebtn.js
@@ -0,0 +1,7 @@
+const title=document.getElementbtId("changebtn");
+const button=document.getElementbyId("changebtn");
+
+button.addeveventlistener("click",function(){
+	title.textcontent="button Clicked!";
+	title.style.colr="red";
+});
